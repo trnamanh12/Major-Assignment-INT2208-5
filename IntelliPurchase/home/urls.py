1c23cfeb4ea9c01b834c4696092db4eb1a88150c
manhbd-22022602 <bdm2k4@gmail.com>
2024-05-01 23:55:07 +0700

    Update page sign up account

--- IntelliPurchase/home/urls.py
+++ IntelliPurchase/home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
    path('', views.index),
    path('/contact/', views.contact, name='contact'),
+   path('register/', views.register, name="register"),
+
 ]
--- IntelliPurchase/home/views.py
+++ IntelliPurchase/home/views.py
@@ -9,3 +9,15 @@ def contact(request):
 
 def error(request, exception):
    return render(request, 'pages/error.html', {'message': exception})
+
+from .forms import RegistrationForm
+from django.http import HttpResponseRedirect 
+
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'pages/register.html', {'form': form})
