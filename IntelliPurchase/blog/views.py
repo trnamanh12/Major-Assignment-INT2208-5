b993fb71207015f55bd7f19b4bf32b902bb06680
manhbd-22022602 <bdm2k4@gmail.com>
2024-05-03 23:39:41 +0700

    Update generic view for page

--- IntelliPurchase/blog/views.py
+++ IntelliPurchase/blog/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
 from .models import Product, Post
-from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 # def list(request):
 #     Data = {'Products': Product.objects.all().order_by('-product_id')}
 #     return render(request, 'blog/blog.html', Data)
-def list(request):
-    Data = {'Posts': Post.objects.all().order_by('-date')}
-    return render(request, 'blog/blog.html', Data)
 
-def post(request, id):
-    try:
-        post = Post.objects.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Bài viết không tồn tại")
-    
-    return render(request, 'blog/post.html', {'post': post})
+class PostListView(ListView):
+    queryset = Post.objects.all().order_by('-date')
+    template_name = 'blog/blog.html'
+    context_object_name = 'Posts'
+    paginate_by = 1
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post.html'
