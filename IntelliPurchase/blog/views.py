9eabaca366781a83524b13b7a70d8b1c81575b70
manhbd-22022602 <bdm2k4@gmail.com>
2024-05-04 22:59:30 +0700

    Add input features

--- IntelliPurchase/blog/views.py
+++ IntelliPurchase/blog/views.py
@@ -16,3 +16,19 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post.html'
+
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from .forms import CommentForm
+from django.http import HttpResponseRedirect
+
+def post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST, author=request.user, post=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path)
+    return render(request, "blog/post.html", {"post": post, "form": form})
+
