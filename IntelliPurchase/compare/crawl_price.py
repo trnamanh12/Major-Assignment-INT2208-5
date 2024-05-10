c2b969e3cbef09dab3c8011eab582bf3463545b8
LeVietHung22022666 <127644242+HungLV2512@users.noreply.github.com>
2024-05-11 01:05:46 +0700

    Update

--- IntelliPurchase/compare/crawl_price.py
+++ IntelliPurchase/compare/crawl_price.py
@@ -27,7 +27,8 @@ def crawl_tgdd(link):
     chrome_options.add_argument("--log-level=3")
 
     # Khởi tạo webdriver với tùy chọn trên và truy cập vào đường link sản phẩm
-    driver = webdriver.Chrome(options=chrome_options, executable_path="Major-Assignment-INT2208-5\IntelliPurchase\compare\chromedriver.exe")
+    chromedriver_path = "Major-Assignment-INT2208-5/IntelliPurchase/compare/chromedriver.exe"
+    driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
     driver.get(link)
 
     css_selectors = [
@@ -70,7 +71,8 @@ def crawl_fpt(link):
     chrome_options.add_argument("--headless")
 
     # Khởi tạo webdriver với tùy chọn trên và truy cập vào đường link sản phẩm
-    driver = webdriver.Chrome(options=chrome_options, executable_path="Major-Assignment-INT2208-5\IntelliPurchase\compare\chromedriver.exe")
+    chromedriver_path = "Major-Assignment-INT2208-5/IntelliPurchase/compare/chromedriver.exe"
+    driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
     driver.get(link)
 
     # Kiểm tra và lấy giá của sản phẩm
